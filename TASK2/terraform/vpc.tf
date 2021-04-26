51eae82c1a8b11adb2a03646f0f2c07e9782a3bc
zahidul12 <islam.md@bkash.com>
2021-04-26 10:50:53 +0600

    vpc and subnet pulling using data

---
+++ TASK2/terraform/vpc.tf
@@ -0,0 +1,21 @@
+data "aws_vpc" "recurring" {
+  id = local.internal-vpc
+}
+data "aws_subnet" "public-subnet-1a" {
+  id = local.public-subnet-block-1a
+}
+data "aws_subnet" "public-subnet-1b" {
+  id = local.public-subnet-block-1b
+}
+data "aws_subnet" "public-subnet-1c" {
+  id = local.public-subnet-block-1c
+}
+data "aws_subnet" "private-subnet-1a" {
+  id = local.private-subnet-block-1a
+}
+data "aws_subnet" "private-subnet-1b" {
+  id = local.private-subnet-block-1b
+}
+data "aws_subnet" "private-subnet-1c" {
+  id = local.private-subnet-block-1c
+}
